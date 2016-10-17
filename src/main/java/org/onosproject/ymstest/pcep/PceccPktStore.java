c2bc5078864ae843642fac15d1df3269e3cfba00
Sojan Koshy <sojan.koshy@huawei.com>
2016-10-17 16:02:46 +0530

    Added pcecc yang

---
+++ src/main/java/org/onosproject/ymstest/pcep/PceccPktStore.java
@@ -0,0 +1,98 @@
+package org.onosproject.ymstest.pcep;
+
+import org.onosproject.yang.gen.v1.urn.ns.yang.pcep.pcecc.rev20161005.Pcecc;
+import org.onosproject.yang.gen.v1.urn.ns.yang.pcep.pcecc.rev20161005.pcecc.PceccPkt;
+import org.onosproject.yang.gen.v1.urn.ns.yang.pcep.pcecc.rev20161005.pcecc.pceccmsg.Response;
+import org.onosproject.yang.gen.v1.urn.ns.yang.pcep.pcecc.rev20161005.pcecc.pceccpkt.Networklist;
+import org.onosproject.yang.gen.v1.urn.ns.yang.pcep.pcecc.rev20161005.pcecc.pceccpkt.Responses;
+import org.onosproject.yang.gen.v1.urn.ns.yang.pcep.types.rev20161005.pceptypes.endpoints.AddressFamily;
+import org.onosproject.yang.gen.v1.urn.ns.yang.pcep.types.rev20161005.pceptypes.requesttlv.Request;
+
+import java.util.BitSet;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by sojan on 17/10/16.
+ */
+public class PceccPktStore implements PceccPkt {
+    @Override
+    public short natureOfIssue() {
+        return 0;
+    }
+
+    @Override
+    public boolean unsatisfiedConstraints() {
+        return false;
+    }
+
+    @Override
+    public boolean ignore() {
+        return false;
+    }
+
+    @Override
+    public BitSet valueLeafFlags() {
+        return null;
+    }
+
+    @Override
+    public Pcecc.OnosYangOpType yangPceccPktOpType() {
+        return null;
+    }
+
+    @Override
+    public BitSet selectLeafFlags() {
+        return null;
+    }
+
+    @Override
+    public Map<Class<?>, Object> yangAugmentedInfoMap() {
+        return null;
+    }
+
+    @Override
+    public Responses responses() {
+        return null;
+    }
+
+    @Override
+    public List<Networklist> networklist() {
+        return null;
+    }
+
+    @Override
+    public Response response() {
+        return null;
+    }
+
+    @Override
+    public Request request() {
+        return null;
+    }
+
+    @Override
+    public AddressFamily addressFamily() {
+        return null;
+    }
+
+    @Override
+    public Object yangAugmentedInfo(Class classObject) {
+        return null;
+    }
+
+    @Override
+    public PceccPkt processSubtreeFiltering(PceccPkt pceccPkt, boolean isSelectAllSchemaChild) {
+        return null;
+    }
+
+    @Override
+    public boolean isLeafValueSet(LeafIdentifier leaf) {
+        return false;
+    }
+
+    @Override
+    public boolean isSelectLeaf(LeafIdentifier leaf) {
+        return false;
+    }
+}
---
+++ src/main/java/org/onosproject/ymstest/pcep/PceccStore.java
@@ -0,0 +1,35 @@
+package org.onosproject.ymstest.pcep;
+
+
+import org.onosproject.yang.gen.v1.urn.ns.yang.pcep.pcecc.rev20161005.Pcecc;
+import org.onosproject.yang.gen.v1.urn.ns.yang.pcep.pcecc.rev20161005.pcecc.DefaultPceccPkt;
+import org.onosproject.yang.gen.v1.urn.ns.yang.pcep.pcecc.rev20161005.pcecc.IpAddress;
+import org.onosproject.yang.gen.v1.urn.ns.yang.pcep.pcecc.rev20161005.pcecc.PceccPkt;
+
+/**
+ * Created by sojan on 17/10/16.
+ */
+public class PceccStore implements Pcecc {
+
+    private PceccPktStore pceccPkt = new PceccPktStore();
+
+    @Override
+    public OnosYangOpType yangPceccOpType() {
+        return null;
+    }
+
+    @Override
+    public PceccPkt pceccPkt() {
+        return null;
+    }
+
+    @Override
+    public IpAddress ipAddress() {
+        return null;
+    }
+
+    @Override
+    public Pcecc processSubtreeFiltering(Pcecc pcecc, boolean isSelectAllSchemaChild) {
+        return null;
+    }
+}
